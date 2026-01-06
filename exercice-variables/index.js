a18127dff70d2dfc12c5e46f5c468bd7cb25c9da
Houss35Code <h.halifa035@gmail.com>
2026-01-06 14:32:50 +0000

    exercice-variables

---
+++ exercice-variables/index.js
@@ -0,0 +1,22 @@
+const character = "Legolas";
+console.log(character);
+
+let name = "Legolas";
+console.log(name);
+
+name = "Gimli";
+console.log(name);
+
+let number = 350;
+console.log(number);
+
+let number1 = 0.233333;
+console.log(number1);
+
+let number2 = -2;
+console.log(number2);
+
+let yes = true;
+console.log(yes);
+let no = false;
+console.log(no);
